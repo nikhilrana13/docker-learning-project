cd20cf15fe85276ac74db2da4d82d22f1c4ac117
nikhilrana13 <nikhilrajput060@gmail.com>
2026-06-15 08:54:22 +0530

    chore: initialize backend package.json with dependencies and scripts

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express") 
+const dotenv = require("dotenv")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 4000 
+const app = express()
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())  
+
+
+// routes
+app.get("/",(req,res)=>{
+    res.send("hello from backend")
+})
+
+// db 
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
+
+
+
+
